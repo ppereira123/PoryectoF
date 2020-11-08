e8f2be3b23b258a437afd9916e200771b83a6305
robertvala <robavala@espol.edu.ec>
2020-11-07 21:27:37 -0500

    Inicio de programa

---
+++ Proyecto2/src/main/java/controlador/proyecto2/SecondaryController.java
@@ -0,0 +1,12 @@
+package controlador.proyecto2;
+
+import java.io.IOException;
+import javafx.fxml.FXML;
+
+public class SecondaryController {
+
+    @FXML
+    private void switchToPrimary() throws IOException {
+        App.setRoot("primary");
+    }
+}
---
+++ Proyecto2/src/main/java/module-info.java
@@ -0,0 +1,9 @@
+/* doesn't work with source level 1.8:
+module controlador.proyecto2 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    opens controlador.proyecto2 to javafx.fxml;
+    exports controlador.proyecto2;
+}
+*/
